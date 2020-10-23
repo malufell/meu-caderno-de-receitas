e42397ee1ea32a55489fbe65ca33229c75bfb4be
Malu <malufell@hotmail.com>
2020-10-23 16:22:31 -0300

    implementa autenticação com passport, express-session, connect-flash

--- app/controllers/usuarios-controller.js
+++ app/controllers/usuarios-controller.js
@@ -1,14 +1,27 @@
 const database = require('../models');
 
 class Usuarios {
-        static async buscaUsuarios(req, resp) {
+    static async buscaUsuarios(req, resp) {
         try {
             const usuarios = await database.Usuarios.findAll();
-            return resp.render('usuarios', {usuarios});
+            return resp.render('usuarios', { usuarios });
         } catch (error) {
-            return resp.render('error');
+            return resp.render('error', { error, message: error.message });
         };
-    }; 
+    };
+
+    static async buscaUmUsuario(req, resp) {
+        const { email } = req.params;
+        try {
+            const usuario = await database.Usuarios.findOne({ where: { email: email }});
+            console.log(usuario)
+            return resp.render('dados-cadastro', { usuario: usuario });
+        } catch (error) {
+            return resp.render('error', { error, message: error.message });
+        };
+    };
 };
 
 module.exports = Usuarios;
+
+//passar o ID na req ao invés de e-mail...
--- app/server.js
+++ app/server.js
@@ -5,17 +5,90 @@ const createError = require('http-errors');
 const routes = require('./routes');
 const app = express();
 const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+const passport = require('passport')
+const Strategy = require('passport-local').Strategy;
+const database = require('./models')
+const sessao = require('express-session');
+const flash = require('connect-flash')
+
+app.use(sessao({
+    secret: "aguiareal",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+    genid: function (req) {
+        return uuidv4();
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+routes(app);
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-routes(app);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+//configuração da autenticação e seção
+passport.use('local', new Strategy(
+    {
+        usernameField: 'email',
+        passwordField: 'senha',
+    },
+    function (email, senha, done) {
+        database.Usuarios.findOne({ where: { email: email } }).then(function (usuario) {
+            if (!usuario) {
+                return done(null, false, { message: 'usuário não encontrado' });
+            }
+            if (usuario.senha != senha) {
+
+                return done(null, false, { message: 'senha inválida' });
+            }
+            console.log('tudo certo')
+            return done(null, usuario)
+        })
+    }
+))
+
+passport.serializeUser(function (usuario, done) {
+    done(null, usuario.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    database.Usuarios.findOne({ where: { id: id } }).then(function (usuario) {
+        done(null, usuario);
+    });
+});
+
+app.post('/login', passport.authenticate('local', {
+    failureFlash: true,
+    failureRedirect: '/login',
+}), (req, res, next) => {
+    res.redirect('/dados-cadastro/' + req.user.email);
+})
+
+app.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+        res.redirect('/')
+    })
+});
+
+app.get('/autenticada', function (req, res, next) {
+    if (req.isAuthenticated()) {
+        res.render('autenticada');
+    } else {
+        res.redirect('/login');
+    }
+});
 
 //erros
 app.use((req, res, next) => {
