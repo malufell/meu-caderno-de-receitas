9c506577d77fe55a4f2f35541296dfb0d40e99ed
Malu <malufell@hotmail.com>
2020-10-23 20:30:53 -0300

    refatora código da sessão de autenticação

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 router.get('/login', (req, res, next) => {
     const errors = req.flash().error || [];
     res.render('login', { errors });
 });
+
+
+router.post('/login', passport.authenticate('local', {
+    failureFlash: true,
+    failureRedirect: '/login',
+}), (req, res, next) => {
+    res.redirect('/dados-cadastro/' + req.user.email);
+})
+
+
+router.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+        res.redirect('/')
+    })
+});
+
 
 module.exports = router;
--- app/routes/usuarios.js
+++ app/routes/usuarios.js
@@ -6,4 +6,13 @@ router.get('/usuarios', Usuarios.buscaUsuarios);
 
 router.get('/dados-cadastro/:email', Usuarios.buscaUmUsuario);
 
+//rota utilizada para teste da autenticação
+router.get('/autenticada', function (req, res, next) {
+    if (req.isAuthenticated()) {
+        res.render('autenticada');
+    } else {
+        res.redirect('/login');
+    }
+});
+
 module.exports = router;
