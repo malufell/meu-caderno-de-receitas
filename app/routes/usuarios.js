057b9c2c3116e4beb352e33a0f749db4c439586a
Malu <malufell@hotmail.com>
2020-10-27 13:42:58 -0300

    implementa header e footer layout EJS

--- app/routes/usuarios.js
+++ app/routes/usuarios.js
@@ -6,10 +6,11 @@ router.get('/usuarios', Usuarios.buscaUsuarios);
 
 router.get('/dados-cadastro/:email', Usuarios.buscaUmUsuario);
 
+
 //rota utilizada para teste da autenticação
 router.get('/autenticada', function (req, res, next) {
     if (req.isAuthenticated()) {
-        res.render('autenticada');
+        res.render('autenticada', { usuario: req.user });
     } else {
         res.redirect('/login');
     }
