2d026d26cc5252ae42f0dabe2891d268ea8358e8
Malu <malufell@hotmail.com>
2022-11-28 23:34:26 -0300

    feat: add new env var

--- app/config/config.js
+++ app/config/config.js
@@ -1,29 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    "development": {
-      "username": "postgres",
-      "password": "admin",
-      "database": "receitas",
-      "host": "127.0.0.1",
-      "dialect": "postgres"
+  development: {
+    username: "postgres",
+    password: "admin",
+    database: "receitas",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
     },
-    "test": {
-      "username": "root",
-      "password": null,
-      "database": "database_test",
-      "host": "127.0.0.1",
-      "dialect": "postgres"
-    },
-    "production": {
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASS,
-      "database": process.env.DB_NAME,
-      "host": process.env.DB_HOST,
-      "dialect": "postgres",
-      "protocol": "postgres",
-      "dialectOptions": {
-        "ssl": { rejectUnauthorized: false }
-      }
-    }  
-  }
+  },
+};
